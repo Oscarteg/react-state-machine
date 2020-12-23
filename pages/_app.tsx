65b94c7fe30deb968f2878930f5b4427016230fd
Oscar te Giffel <oscar@tegiffel.com>
2020-12-23 15:24:41 +0100

    Added some dependencies and tried to fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { inspect } from "@xstate/inspect";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+
+if (typeof window !== "undefined") {
+  inspect({ iframe: false });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
